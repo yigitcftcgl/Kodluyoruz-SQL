d30b4603066d24bf083913928b00af5367da075c
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-08-16 16:52:22 +0300

    Add files via upload

---
+++ ODEV4.sql
@@ -0,0 +1,9 @@
+-- SELECT DISTINCT replacement_cost FROM film
+
+-- SELECT COUNT(DISTINCT replacement_cost) FROM film
+
+-- SELECT COUNT(*) FROM film WHERE (rating='G') AND (title LIKE '%T')
+
+-- SELECT COUNT(country) FROM country WHERE country LIKE '_____'
+
+-- SELECT COUNT(city) FROM city WHERE city ~~* '%r'
