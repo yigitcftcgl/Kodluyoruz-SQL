c217c3c3ab142d77f1be8e2de5b0c9c16afb2c78
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-08-26 15:38:33 +0300

    Add files via upload

---
+++ odev7.sql
@@ -0,0 +1,8 @@
+-- select rating,count(*) from film group by rating
+
+-- select replacement_cost,count(*) from film group by replacement_cost having count(*) > 50;
+
+-- select store_id,count(store_id) from customer group by store_id;
+
+-- select country_id,count(country_id) as "sehir_sayisi" from city group by country_id order by count(country_id) desc limit 1;
+
