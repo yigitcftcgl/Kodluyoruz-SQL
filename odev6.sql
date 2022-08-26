5a2de964a0eb95f8d1bc1a7362826eeb6c37c7fd
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-08-26 14:46:02 +0300

    Add files via upload

---
+++ odev6.sql
@@ -0,0 +1,7 @@
+-- select avg(rental_rate)  from film;
+
+-- select * from film where title ilike 'c%';
+
+-- select * from film where rental_rate = 0.99 order by length desc limit 1;
+
+-- select count(distinct replacement_cost) from film where length > 150;
