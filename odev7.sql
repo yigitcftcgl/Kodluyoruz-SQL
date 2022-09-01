ebbc31e1bdd74c7922b36f6b69752fc17622f959
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-09-02 01:14:04 +0300

    Update odev7.sql

--- odev7.sql
+++ odev7.sql
@@ -1,3 +1,16 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+2.film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+
+Kolay Gelsin.
+
+*/
+
 -- select rating,count(*) from film group by rating
 
 -- select replacement_cost,count(*) from film group by replacement_cost having count(*) > 50;
