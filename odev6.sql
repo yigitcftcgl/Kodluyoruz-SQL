110d4d1646e78956b782ed920cd8d88ec858fe5c
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-09-02 01:15:00 +0300

    Update odev6.sql

--- odev6.sql
+++ odev6.sql
@@ -1,3 +1,15 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+2.film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+4.film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+Kolay Gelsin.
+*/
+
 -- select avg(rental_rate)  from film;
 
 -- select * from film where title ilike 'c%';
