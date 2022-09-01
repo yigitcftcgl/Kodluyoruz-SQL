d55eb4cfbce000c5c64ae6cd212e9407071b8c31
Tufan Yiğit Çiftçioğlu <59434876+yigitcftcgl@users.noreply.github.com>
2022-09-02 01:15:43 +0300

    Update odev5.sql

--- odev5.sql
+++ odev5.sql
@@ -1,3 +1,14 @@
+/*
+Merhabalar,
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+1.film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+2.film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+3.customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+Kolay Gelsin.
+*/
+
 -- select * from film where title like '%n' order by length desc limit 5;
 
 -- select * from film where title like '%n' order by length offset 5 limit 5;
